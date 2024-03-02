3c8a0a56706c7c57a0188b86612db33c3a591bac
PatchedDev <danila.kardashevskii@icloud.com>
2024-03-02 18:48:45 +0100

    classwork try commit

--- in_class/classwork1.py
+++ in_class/classwork1.py
@@ -1,3 +1,21 @@
-s = "abc"
-s = s.upper
-print(s)
+#classwork
+
+def task_1(n):
+
+    if len(n) == 6:
+             first = int(n[0])
+             second = int(n[1])
+             third = int(n[2])
+             fourth = int(n[3])
+             fifth = int(n[4])
+             sixth = int(n[5])
+
+             if first * third + 2 + sixth == second + fourth * fifth:
+               print(True)
+             else:
+                print(False)
+    else:
+        print('wrong number')
+
+
+task_1(input('Enter a 6 digit number: '))
